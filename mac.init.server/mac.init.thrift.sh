f2abfa9ec67c6ba14d98b352f23ec3a098123f56
mmfei <wlfkongl@163.com>
2015-03-05 23:46:06 +0800

    edit zendOpcode & thrift

--- mac.init.server/mac.init.thrift.sh
+++ mac.init.server/mac.init.thrift.sh
@@ -1,6 +1,6 @@
 wget http://mirrors.cnnic.cn/apache/thrift/0.9.2/thrift-0.9.2.tar.gz;
 tar xvzf thrift-0.9.2.tar.gz;
 cd thrift-0.9.2;
-./configure --with-java --with-php --with-python;
+./configure --with-java --with-php --with-python --prefix=/usr/local/thrift.0.9.2;
 make;
 make install;
