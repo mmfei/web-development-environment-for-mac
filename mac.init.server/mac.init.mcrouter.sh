e240554a4609a7f175bf92f0f47fcd40aee95627
mmfei <wlfkongl@163.com>
2015-03-05 23:45:09 +0800

    add mcrouter

---
+++ mac.init.server/mac.init.mcrouter.sh
@@ -0,0 +1,21 @@
+pwd=`pwd`;
+git clone https://github.com/facebook/folly.git;
+cd folly;
+#for linux
+#autoreconf -ivf;
+#./configure --prefix=/usr/local/folly;
+
+#for osx
+./bootstrap-osx-homebrew.sh
+make;
+make check;
+make install;
+
+cd $pwd;
+
+git clone https://github.com/facebook/mcrouter.git;
+cd mcrouter/mcrouter;
+autoreconf --install;
+./configure --prefix=/usr/local/mcrouter;
+make;
+make install;
